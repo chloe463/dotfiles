ee604b1256ebb582ac175ba378c74b115f873a1c
Tsuyoshi HARA <chloe463+git@gmail.com>
2019-03-22 01:14:43 +0900

    refactor(bootstrap): Add functions

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,27 +5,44 @@ set -o pipefail
 
 BASE_DIR=$HOME/dotfiles
 
-echo "Making symlinks of prompt setup..."
-echo
+function make_prompt_set_symlinks()
+{
+    echo "Making symlinks of prompt setup..."
+    echo
 
-for f in prompt**
-do
-    CMD="ln -sf $HOME/dotfiles/$f $HOME/.zprezto/modules/prompt/functions/$f"
-    echo $CMD
-    $CMD
-done
+    for f in prompt**
+    do
+        CMD="ln -sf $HOME/dotfiles/$f $HOME/.zprezto/modules/prompt/functions/$f"
+        echo $CMD
+        $CMD
+    done
 
-echo "Making symlinks of dot files..."
-echo
+    echo
+    echo "🎉 Done!"
+    echo
+}
 
-for f in .**?
-do
-    [ "$f" == ".git" ] && continue
-    [ "$f" == ".."   ] && continue
-    [ "$f" == "."    ] && continue
-    [ "$f" == ".DS_Store" ] && continue
-    [ "$f" == ".zshrc" ] && continue
-    echo "ln -sf $BASE_DIR/$f ~/$f"
-    ln -sf $BASE_DIR/$f ~/$f
-done
+function make_dot_files_symlinks()
+{
+    echo "Making symlinks of dot files..."
+    echo
+
+    for f in .**?
+    do
+        [ "$f" == ".git" ] && continue
+        [ "$f" == ".."   ] && continue
+        [ "$f" == "."    ] && continue
+        [ "$f" == ".DS_Store" ] && continue
+        [ "$f" == ".zshrc" ] && continue
+        echo "ln -sf $BASE_DIR/$f ~/$f"
+        ln -sf $BASE_DIR/$f ~/$f
+    done
+
+    echo
+    echo "🎉 Done!"
+    echo
+}
+
+make_prompt_set_symlinks
+make_dot_files_symlinks
 
