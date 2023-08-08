c883b86622c4987b45582f93b2858c760825e53c
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-08-09 00:38:08 +0900

    fix(nvim): Fix lspsaga configuration

--- nvim/.config/nvim/after/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/after/plugin/lspsaga.rc.lua
@@ -20,6 +20,7 @@ saga.setup({
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', '<Leader>ld', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
-vim.keymap.set('n', '<Leader>pr', '<Cmd>Lspsaga peek_definition<CR>', opts)
-vim.keymap.set('n', '<F2>', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set('n', '<Leader>pr', '<Cmd>Lspsaga finder<CR>', { desc = '[P]eek [R]eferences' })
+vim.keymap.set('n', '<Leader>pd', '<Cmd>Lspsaga peek_definition<CR>', { desc = '[P]eek [D]efinition' })
+vim.keymap.set('n', '<Leader>rn', '<Cmd>Lspsaga rename<CR>', { desc = '[R]e[N]ame' })
 
