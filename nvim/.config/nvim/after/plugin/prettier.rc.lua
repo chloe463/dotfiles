cc48ef3271c4292025bf70a9c4956b1aa225f50a
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-08-12 00:41:09 +0900

    fix(nvim): Add comment

--- nvim/.config/nvim/after/plugin/prettier.rc.lua
+++ nvim/.config/nvim/after/plugin/prettier.rc.lua
@@ -2,6 +2,8 @@ local status, prettier = pcall(require, 'prettier')
 if (not status) then return end
 
 prettier.setup {
+  -- You need to install 'prettierd' first.
+  -- yarn global add prettierd
   bin = 'prettierd',
   filetypes = {
     'css',
