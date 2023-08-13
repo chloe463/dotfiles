2059e49f0bc6f07ae11b932ae4da71ad87b91efe
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-08-13 11:23:17 +0900

    fix(nvim): Display diagnostic info on bufferline

--- nvim/.config/nvim/after/plugin/bufferline.rc.lua
+++ nvim/.config/nvim/after/plugin/bufferline.rc.lua
@@ -3,12 +3,34 @@ if (not status) then return end
 
 bufferline.setup({
   options = {
-    mode = "tabs",
+    mode = 'tabs',
     separator_style = 'thick',
     always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
+    indicator = {
+      style = 'underline',
+    },
+    diagnostics = 'nvim_lsp',
+    diagnostics_indicator = function(_count, _level, diagnostics_dict)
+      local error_icon = ' '
+      local warning_icon = ' '
+      local error_count = diagnostics_dict.error
+      local warning_count = diagnostics_dict.warning
+
+      local message = ''
+      if error_count and error_count > 0 then
+        local error_message = ' ' .. error_icon .. error_count
+        message = message .. error_message
+      end
+      if warning_count and warning_count > 0 then
+        local warning_message = ' ' .. warning_icon .. warning_count
+        message = message .. warning_message
+      end
+      return message
+    end,
+    tab_size = 24,
   },
 })
 
