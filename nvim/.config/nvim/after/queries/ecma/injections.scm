a9be3890835cb5ea6056c52d618ad18f9aa0c67a
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-09-01 15:01:00 +0900

    fix(nvim): Fix graphql syntax highlight configuration

--- nvim/.config/nvim/after/queries/ecma/injections.scm
+++ nvim/.config/nvim/after/queries/ecma/injections.scm
@@ -8,3 +8,16 @@
   (template_string) @injection.content
   (#set! injection.language "graphql"))
 
+(call_expression
+ function: ((identifier) @_name
+   (#eq? @_name "gql"))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "graphql")))
+
+(call_expression
+ function: ((identifier) @_name
+   (#eq? @_name "graphql"))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "graphql")))
