6416463c777757feb4ccf16d331f2258658eb32a
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-09-17 00:34:13 +0900

    fix(nvim): Fix colorizer configuration

--- nvim/.config/nvim/after/plugin/colorizer.rc.lua
+++ nvim/.config/nvim/after/plugin/colorizer.rc.lua
@@ -1,4 +1,15 @@
 local status, colorizer = pcall(require, 'colorizer');
 if (not status) then return end
 
-colorizer.setup()
+colorizer.setup {
+  '*',
+  typescript = {
+    rgb_fn = true,
+  },
+  javascript = {
+    rgb_fn = true,
+  },
+  css = {
+    rgb_fn = true,
+  },
+}
